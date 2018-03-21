bf65cb77ed145bc511995fe71dbde82976f810d0
vinayBomma <vinaysb2000@gmail.com>
2018-03-21 22:37:11 +0530

    Cleared Bugs.Date cannot be in past now.Values get cleared after adding tasks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-//----------------------  JQUERY INIT FOR MATERIALIZE --------------------------
+// TODO WHEN NO TASKS ARE ADDED, THERE SHOULD BE SOME TEXT IN CENTER, SHOULDNT LOOK EMPTY
+// TODO ADD MINIMUM $ MAXIMUM HEIGHT TO TASKS CARD
+// TODO OPTION TO SELECT COLOR OF TASKS CARD
+// TODO DELETE TASKS OPTION
+// TODO EDIT TASKS OPTION
+// TODO UNDO TASK CREATION WITH TOAST
+// TODO TIMEPICKER FOR DEADLINE OF TASK
+// TODO ADD FLOATING BUTTON FOR ADDING TASKS
+// TODO SORTING OPTIONS
+// TODO RESPONSIVE PAGE
+// TODO LOCAL STORAGE
+// TODO TEST APP WITH COOKIE DELETER
+// TODO ANIMATIONS
+// TODO USE THAT AUTOMATIC TYPER PLUGIN
+// TODO PUNISHING FOR NOT COMPLETING BEFORE DEADLINE
+// TODO IMPLEMENT MODULE PATTERN
+// TODO MAKE IT A PROGRESSIVE WEB APP (PWA)
+
+//----------------------  JQUERY INIT FOR MATERIALIZE --------------------------cd cd CW
 
 $(document).ready(function () {
-    $(".modal").modal();
-    $('select').material_select();
+    $('.modal').modal({
+        dismissible: true,
+    });
 
+    $('select').material_select();
 
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
@@ -12,7 +32,7 @@ $(document).ready(function () {
         clear: 'Clear',
         close: 'Ok',
         closeOnSelect: false, // Close upon selecting a date,
-        format: 'dd/mm/yyyy'
+        format: 'mm/dd/yyyy'
     });
 
     // $('.timepicker').pickatime({
@@ -38,7 +58,7 @@ const task_card = document.querySelector('.tasks_card');
 
 let card, card_div, card_sub_div, card_title, card_des, card_date, card_priority;
 
-// ----------------------------------  Creating DIV Structure ---------------------------------------
+// ----------------------------- Creating DIV Structure For Adding Tasks ---------------------------------------
 
 function createDivStructure() {
     card = document.createElement('div');
@@ -76,23 +96,49 @@ function addTask() {
     task_card.appendChild(card)
 }
 
+// --------------------------------  Clears Values After Task Creation -------------------------------------
+
+function clear_values() {
+    taskName.value = '';
+    deadlineDay.value = '';
+    description.value = '';
+    priority.value = '';
+}
+
+// -------------------------------- Validates Inputs in Form ------------------------------------
+
 function form_validator() {
-    if (taskName.value === ''){
-        Materialize.toast('Please Name Your Task', 2000, 'rounded')
-    }else if(deadlineDay.value === ''){
-        Materialize.toast('Please Add Deadline Day', 2000, 'rounded')
-    }else if(priority.value === ''){
-        Materialize.toast('Please Add Priority', 2000, 'rounded')
-    }else{
+
+    if (taskName.value === '') {
+        Materialize.toast('Please Name Your Task', 2000, 'rounded');
+        submitBtn.classList.remove('modal-close')
+
+    } else if (deadlineDay.value === '') {
+        Materialize.toast('Please Add Deadline Day', 2000, 'rounded');
+        submitBtn.classList.remove('modal-close')
+
+    } else if ((new Date(deadlineDay.value)) < new Date()) {
+        Materialize.toast('Date Cannot Be In Past', 2000, 'rounded')
+        submitBtn.classList.remove('modal-close')
+
+    } else if (description.value.length > 70) {
+        Materialize.toast('Description Character Length Exceeded', 2000, 'rounded');
+        submitBtn.classList.remove('modal-close')
+
+    } else if (priority.value === '') {
+        Materialize.toast('Please Add Priority', 2000, 'rounded');
+        submitBtn.classList.remove('modal-close')
+
+    } else {
+        submitBtn.classList.add('modal-close');
         createDivStructure();
         addTask();
+        clear_values();
         Materialize.toast('New Task Created', 2000, 'rounded')
     }
 }
 
-submitBtn.addEventListener('click', function () {
-    form_validator();
-});
+submitBtn.addEventListener('click', form_validator);
 
 
 
