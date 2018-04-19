47222ab11a007dc2fa2952107f8b9ded74df5ebe
vinayBomma <vinaysb2000@gmail.com>
2018-04-19 13:05:11 +0530

    Made few changes to service worker

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ self.addEventListener('install', function (e) {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/index.html?homescreen=1',
+                // '/index.html?homescreen=1',
                 '/?homescreen=1',
                 '/css/main.css',
                 '/js/main.js',
